49e93990743c672d7c8d8e7e1a70be21f795d5e4
Aaron Namba <aaron@aaronnamba.com>
2020-09-09 11:48:30 +0900

    fixes for version and build number retrieval

--- lib/imagine/config.ex
+++ lib/imagine/config.ex
@@ -1,8 +1,8 @@
 defmodule Imagine.Config do
-  def version, do: Imagine.MixProject.project()[:version]
+  def version, do: Application.spec(:imagine_cms, :vsn)
 
-  def build_number,
-    do: System.cmd("git", ["rev-list", "HEAD", "--count"]) |> elem(0) |> String.trim()
+  @build_number System.cmd("git", ["rev-list", "HEAD", "--count"]) |> elem(0) |> String.trim()
+  def build_number, do: @build_number
 
   def release_built_at, do: DateTime.utc_now()
 end
